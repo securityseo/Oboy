6fe03eef4d02509dfd3948e8a1ef334df4c50fa0
akmalmadiyarov1 <madiyarovakmal219@gmail.com>
2023-11-30 17:06:15 +0500

    new disign

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,7 +1,14 @@
 import { Link } from 'react-router-dom';
 import Logo from '../Images/logo.png'
 import '../Styles/Header.css'
-
+import {
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  Button,
+} from '@chakra-ui/react'
+import { ChevronDownIcon } from '@chakra-ui/icons';
 const Header = () => {
   return (
     <div>
@@ -27,6 +34,18 @@ const Header = () => {
              <Link to={'/contacts'}>
              <a href="#">Контакты</a>
              </Link>
+             <Menu>
+  <MenuButton color={'#000'}  backgroundColor={'#BC955D'} _hover={''} as={Button}>
+    Actions
+  </MenuButton>
+  <MenuList>
+    <MenuItem>Download</MenuItem>
+    <MenuItem>Create a Copy</MenuItem>
+    <MenuItem>Mark as Draft</MenuItem>
+    <MenuItem>Delete</MenuItem>
+    <MenuItem>Attend a Workshop</MenuItem>
+  </MenuList>
+</Menu>
               </div>
             </div>
           </div>
--- src/Main/Product.jsx
+++ src/Main/Product.jsx
@@ -1,12 +1,48 @@
 import { Tab, TabList, TabPanel, TabPanels, Tabs } from "@chakra-ui/react";
 import LeftLine from "../Images/left.png";
 import RightLine from "../Images/right.png";
-import News from './News'
+import News from "./News";
+import data from "../db/db";
+import { useState } from "react";
 
 
 const Product = () => {
+  const [imgSrc,setImgSrc] = useState('')
+
+  function toggleModal(b,id){
+    const e = document.getElementById('modal')
+    const w = document.getElementById('m_window')
+  
+    if(b){
+      e.style.transform = "scale(1)"
+      w.style.transform = "scaleY(1) translate(-50%,-50%)"
+      const getProduct = data.filter((data) => data.id === id)
+      setImgSrc(getProduct[0].img)
+    }else{
+      e.style.transform = "scale(0)"
+
+    }
+
+  }
 
 
+function ProductSortRender({ brand }) {
+  const sort = data.filter((data) => data.brand === brand);
+  return (
+    <>
+      {sort?.map((v) => {
+        return (
+          <button className="product" key={v.id} onClick={()=>toggleModal(true,v.id)}>
+            <img id="modal_img" src={v.img} alt="" />
+            <div className="product-title">
+              <h2>{v.title}</h2>
+            </div>
+          </button>
+        );
+      })}
+    </>
+  );
+}
 
   return (
     <div>
@@ -17,61 +53,37 @@ const Product = () => {
           <img src={RightLine} alt="" />
         </div>
         <div className="container">
-          <Tabs variant="enclosed" width={'100%'} colorScheme='white'>
-            <TabList display={'flex'}  alignItems={'center'} justifyContent={'space-between'} >
-              <Tab width={'100%'} color={'#BC955D'}>Обои из вспененного винила</Tab>
-              <Tab width={'100%'} color={'#BC955D'} >Обои горячего тиснения</Tab>
-              <Tab width={'100%'}  color={'#BC955D'}>Обои под покраску</Tab>
+          <Tabs variant="enclosed" width={"100%"} colorScheme="white">
+            <TabList
+              display={"flex"}
+              alignItems={"center"}
+              justifyContent={"space-between"}
+            >
+              <Tab width={"100%"} color={"#BC955D"}>
+                Обои из вспененного винила
+              </Tab>
+              <Tab width={"100%"} color={"#BC955D"}>
+                Обои горячего тиснения
+              </Tab>
+              <Tab width={"100%"} color={"#BC955D"}>
+                Обои под покраску
+              </Tab>
             </TabList>
             <TabPanels>
               <TabPanel>
-<div className="product-bag">
-<div className="product">
-<div className="product-title">
-    <h2>Арт. «Марокканские» 1031</h2>
-</div>
-</div>
-<div className="product">
-<div className="product-title">
-    <h2>Арт. «Марокканские» 1031</h2>
-</div>
-</div>
-<div className="product">
-<div className="product-title">
-    <h2>Арт. «Марокканские» 1031</h2>
-</div>
-</div>
-</div>
-
+                <div className="product-bag">
+                  <ProductSortRender brand="vinil" />
+                </div>
               </TabPanel>
               <TabPanel>
-               
-
-<div className="product-bag">
-<div className="product">
-<div className="product-title">
-    <h2>Арт. «Марокканские» 1031</h2>
-</div>
-</div>
-</div>
-
+                <div className="product-bag">
+                  <ProductSortRender brand="tisney" />
+                </div>
               </TabPanel>
               <TabPanel>
-
-<div className="product-bag">
-<div className="product">
-<div className="product-title">
-    <h2>Арт. «Марокканские» 1031</h2>
-</div>
-</div>
-<div className="product">
-<div className="product-title">
-    <h2>Арт. «Марокканские» 1031</h2>
-</div>
-</div>
-
-</div>
-
+                <div className="product-bag">
+                  <ProductSortRender brand="poqraska" />
+                </div>
               </TabPanel>
             </TabPanels>
           </Tabs>
@@ -82,7 +94,7 @@ const Product = () => {
           <img src={RightLine} alt="" />
         </div>
         <div className="container">
-          <News/>
+          <News />
         </div>
         <div className="katalog">
           <img src={LeftLine} alt="" />
@@ -90,6 +102,15 @@ const Product = () => {
           <img src={RightLine} alt="" />
         </div>
       </div>
+
+{/* Modal Window */}
+    <div id="modal">
+      <button id="closeModalBtn" onClick={()=>toggleModal(false,null)}>X</button>
+      <div id="m_window" className="window">
+        <img id="modal_img" src={imgSrc} alt="" />
+      </div>
+    </div>
+
     </div>
   );
 };
