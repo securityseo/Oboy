769e535a57f2e4ed81b59b014d2c966f9a7a6e02
akmalmadiyarov1 <madiyarovakmal219@gmail.com>
2023-12-01 16:42:46 +0500

    e31r4t5y6jukilo;p

--- src/Pages/Onas.jsx
+++ src/Pages/Onas.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
-
-const Onas = () => {
+import "../Styles/News.css";
+import Logo from "../Images/logo.png";
+import { Link } from "react-router-dom";
+const News = () => {
   return (
-    <div>
-      Onas
+    <div className="wrapper">
+      <div className="container">
+        <div className="news-block">
+          <div className="news-img">
+            <img src={Logo} alt="" />
+          </div>
+          <div className="news-title">
+            <h2>Lorem</h2>
+            <h3>
+              Lorem ipsum dolor sit, amet consectetur <br /> adipisicing elit. Laborum, tenetur consectetur! <br /> Quibusdam sint quos minima neque nobis, ipsam, cupiditate temporibus autem.
+            </h3>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Onas
+export default News;
