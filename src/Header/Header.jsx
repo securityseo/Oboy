0a9db8ff4415921ad24adb4f3577361c41bce995
akmalmadiyarov1 <madiyarovakmal219@gmail.com>
2023-12-01 16:33:42 +0500

    oiuyd

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,14 +1,8 @@
-import { Link } from 'react-router-dom';
-import Logo from '../Images/logo.png'
-import '../Styles/Header.css'
-import {
-  Menu,
-  MenuButton,
-  MenuList,
-  MenuItem,
-  Button,
-} from '@chakra-ui/react'
-import { ChevronDownIcon } from '@chakra-ui/icons';
+import { Link } from "react-router-dom";
+import Logo from "../Images/logo.png";
+import "../Styles/Header.css";
+import { Menu, MenuButton, MenuList, MenuItem, Button } from "@chakra-ui/react";
+import { ChevronDownIcon } from "@chakra-ui/icons";
 const Header = () => {
   return (
     <div>
@@ -17,47 +11,83 @@ const Header = () => {
           <div className="container">
             <div className="header-nav">
               <div className="header-logo">
-                <Link to={'/'}>
-                <img src={Logo} alt="" />
+                <Link to={"/"}>
+                  <img src={Logo} alt="" />
                 </Link>
               </div>
               <div className="header-links">
-               <Link to={'/about'}>
-               <a href="#">О нас</a>
-               </Link>
-               <Link to={'/catalog'}>
-               <a href="#">Каталог</a>
-               </Link>
-                <Link to={'/news'}>
-                <a href="#">Новости</a>
+                <Link to={"/about"}>
+                  <a href="#">О нас</a>
                 </Link>
-             <Link to={'/contacts'}>
-             <a href="#">Контакты</a>
-             </Link>
-    
+                <Link to={"/catalog"}>
+                  <a href="#">Каталог</a>
+                </Link>
+                <Link to={"/news"}>
+                  <a href="#">Новости</a>
+                </Link>
+                <Link to={"/contacts"}>
+                  <a href="#">Контакты</a>
+                </Link>
               </div>
-                <div className='responsive-menu'>
-      <Menu>
-  <MenuButton color={'#000'}  backgroundColor={'#BC955D'} _hover={'none'} as={Button}>
-    Menu
-  </MenuButton>
-  <MenuList backgroundColor={'#BC955D'} >
-    <Link to={'/about'}>
-       <MenuItem backgroundColor={'#BC955D'} color={'#fff'}>О нас</MenuItem>
-    </Link>
-    <Link to={'/catalog'}>
-       <MenuItem backgroundColor={'#BC955D'} color={'#fff'}  borderTop={'1px solid #fff'}>Каталог</MenuItem>
-    </Link>
-    <Link to={'/news'}>
-      <MenuItem backgroundColor={'#BC955D'} color={'#fff'}  borderTop={'1px solid #fff'}>Новости</MenuItem>
-    </Link>
-    
-   <Link to={'/contacts'}>
-   <MenuItem backgroundColor={'#BC955D'} color={'#fff'}  borderTop={'1px solid #fff'}>Контакты</MenuItem>
-   </Link>
-  </MenuList>
-</Menu>
-      </div>
+              <div className="responsive-menu">
+                <Menu>
+                  <MenuButton
+                    color={"#000"}
+                    backgroundColor={"#BC955D"}
+                    _hover={"none"}
+                    as={Button}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="26"
+                      height="26"
+                      fill="currentColor"
+                      class="bi bi-text-right"
+                      viewBox="0 0 16 16"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M6 12.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m-4-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5m4-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m-4-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5"
+                      />
+                    </svg>
+                  </MenuButton>
+                  <MenuList backgroundColor={"#BC955D"}>
+                    <Link to={"/about"}>
+                      <MenuItem backgroundColor={"#BC955D"} color={"#fff"}>
+                        О нас
+                      </MenuItem>
+                    </Link>
+                    <Link to={"/catalog"}>
+                      <MenuItem
+                        backgroundColor={"#BC955D"}
+                        color={"#fff"}
+                        borderTop={"1px solid #fff"}
+                      >
+                        Каталог
+                      </MenuItem>
+                    </Link>
+                    <Link to={"/news"}>
+                      <MenuItem
+                        backgroundColor={"#BC955D"}
+                        color={"#fff"}
+                        borderTop={"1px solid #fff"}
+                      >
+                        Новости
+                      </MenuItem>
+                    </Link>
+
+                    <Link to={"/contacts"}>
+                      <MenuItem
+                        backgroundColor={"#BC955D"}
+                        color={"#fff"}
+                        borderTop={"1px solid #fff"}
+                      >
+                        Контакты
+                      </MenuItem>
+                    </Link>
+                  </MenuList>
+                </Menu>
+              </div>
             </div>
           </div>
         </div>
