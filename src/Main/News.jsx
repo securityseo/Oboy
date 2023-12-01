dbea0e23c6e0cdee24089ee3f2925c8696492ada
akmalmadiyarov1 <madiyarovakmal219@gmail.com>
2023-12-01 16:01:44 +0500

    55t5t5

--- src/Main/News.jsx
+++ src/Main/News.jsx
@@ -1,22 +1,25 @@
-import '../Styles/News.css'
-import Logo from '../Images/logo.png'
+import "../Styles/News.css";
+import Logo from "../Images/logo.png";
+import { Link } from "react-router-dom";
 const News = () => {
   return (
     <div className="wrapper">
-        <div className="container">
-                <div className="news-block">
-                   <div className="news-img">
-                   <img src={Logo} alt="" />
-                   </div>
-                   <div className="news-title">
-                    <h2>Дилеры</h2>
-                    <h3>Xorazm viloyati: Urganch Shahar. <br /> Gulobod kòchasi 11
-uy. Oboylar dunyosi dòkoni. <br /> Telefon raqam. +99888 888 88 88 </h3>
-                   </div>
-                </div>
+      <div className="container">
+        <div className="news-block">
+          <div className="news-img">
+            <img src={Logo} alt="" />
+          </div>
+          <div className="news-title">
+            <h2>Дилеры</h2>
+            <h3>
+              Xorazm viloyati: Urganch Shahar. <br /> Gulobod kòchasi 11 uy.
+              Oboylar dunyosi dòkoni. <br /> Telefon raqam. <Link to={'tel:+998904345377'}>+998 (90) 434 53 77</Link>
+            </h3>
+          </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default News
+export default News;
