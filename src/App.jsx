26218a1eb71ebb2489bd41ee3d31539fd75e105a
akmalmadiyarov1 <madiyarovakmal219@gmail.com>
2023-12-02 13:50:39 +0500

    loading

--- src/App.jsx
+++ src/App.jsx
@@ -8,20 +8,56 @@ import Katalog from "./Pages/Katalog";
 import Nowosti from "./Pages/Nowosti";
 import Kontaqti from "./Pages/Kontaqti";
 import Error from "./Pages/404/Error";
+import { useEffect, useState } from "react";
+
+import './Styles/Loading.css'
+
+const Loading = () =>{
+  return(
+    <div className="loaderWrapper">
+  <div class="loader">
+	<div></div>
+	<div></div>
+	<div></div>
+	<div></div>
+	<div></div>
+  <p>Loading...</p>
+</div>
+    </div>
+  )
+}
 
 const App = () => {
+
+  const [isLoading , setIsLoading] = useState(true);
+
+  useEffect(() =>{
+    setTimeout(()=>{
+      setIsLoading(false);
+    },2000);
+  },[])
+
   return (
     <div>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/about" element={<Onas />} />
-        <Route path="/catalog" element={<Katalog />} />
-        <Route path="/news" element={<Nowosti />} />
-        <Route path="/contacts" element={<Kontaqti />} />
-        <Route path="*" element={<Error/>} />
-      </Routes>
-      <Footer />
+        {isLoading ?(
+          <Loading/>
+          
+
+        ) :(
+        <>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/about" element={<Onas />} />
+            <Route path="/catalog" element={<Katalog />} />
+            <Route path="/news" element={<Nowosti />} />
+            <Route path="/contacts" element={<Kontaqti />} />
+            <Route path="*" element={<Error/>} />
+          </Routes>
+          <Footer />
+        </>
+        )
+        }
     </div>
   );
 };
