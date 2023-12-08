a4a9bb89e45e7f129a6930f4416abe57af2a7927
akmalmadiyarov1 <madiyarovakmal219@gmail.com>
2023-12-08 10:57:52 +0500

    asd

--- src/App.jsx
+++ src/App.jsx
@@ -2,47 +2,44 @@ import { Route, Routes } from "react-router-dom";
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
 import Main from "./Main/Main";
-import './Styles/Normalize.css';
+import "./Styles/Normalize.css";
 import Onas from "./Pages/Onas";
 import Katalog from "./Pages/Katalog";
 import Kontaqti from "./Pages/Kontaqti";
 import Error from "./Pages/404/Error";
 import { useEffect, useState } from "react";
 
-import './Styles/Loading.css'
+import "./Styles/Loading.css";
 
-const Loading = () =>{
-  return(
+const Loading = () => {
+  return (
     <div className="loaderWrapper">
-  <div class="loader">
-	<div></div>
-	<div></div>
-	<div></div>
-	<div></div>
-	<div></div>
-  <p>hali ko'p kutasan...</p>
-</div>
+      <div class="loader">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <p>Loading...</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
 
-  const [isLoading , setIsLoading] = useState(true);
-
-  useEffect(() =>{
-    setTimeout(()=>{
+  useEffect(() => {
+    setTimeout(() => {
       setIsLoading(false);
-    },3);
-  },[])
+    }, 1000);
+  }, []);
 
   return (
     <div>
-        {isLoading ?(
-          <Loading/>
-          
-
-        ) :(
+      {isLoading ? (
+        <Loading />
+      ) : (
         <>
           <Header />
           <Routes>
@@ -50,12 +47,11 @@ const App = () => {
             <Route path="/about" element={<Onas />} />
             <Route path="/catalog" element={<Katalog />} />
             <Route path="/contacts" element={<Kontaqti />} />
-            <Route path="*" element={<Error/>} />
+            <Route path="*" element={<Error />} />
           </Routes>
           <Footer />
         </>
-        )
-        }
+      )}
     </div>
   );
 };
--- src/Main/Banner.jsx
+++ src/Main/Banner.jsx
@@ -1,16 +1,16 @@
-import '../Styles/Banner.css'
+import "../Styles/Banner.css";
 const Banner = () => {
   return (
-    <div className='banner'>
+    <div className="banner">
       <div className="container">
-      <div className="title">
-        <div className="border">
-      <h1>Эмоции вашего восприятия</h1>
-      <p>Лучшее оформление вашего дома</p>
+        <div className="title">
+          <div className="border">
+            <h1>Эмоции вашего восприятия</h1>
+            <p>Лучшее оформление вашего дома</p>
+          </div>
         </div>
       </div>
-      </div>
     </div>
-  )
-}
-export default Banner
+  );
+};
+export default Banner;
